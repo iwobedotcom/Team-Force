30e52eca9b14eb80520b311a1462df38a842c0e3
Iwobe Aigboje <iwobe.aigboje@gmail.com>
2024-09-30 06:35:24 +0100

    Favicon added

--- src/config/metadata.ts
+++ src/config/metadata.ts
@@ -1,10 +1,10 @@
 import { MetadataProp } from '../types';
 
 export const metadata: MetadataProp = {
-  title: '🦊 Team Fox - Scan & Go, Easy Invoicing, and Secure Payment Methods',
+  title: 'Team Fox - Scan & Go, Easy Invoicing, and Secure Payment Methods',
   description:
     "Discover a seamless way to manage your payments with our next-generation payment platform. Featuring powerful tools like Scan & Go, online billing, and support for all major payment methods. Whether you're sending requests via email, managing finances, or using bank transfers, our platform is designed for simplicity and security. Join millions who trust us for reliable and efficient payment solutions.",
-  applicationName: '🦊 Team Fox',
+  applicationName: 'Team Fox',
   referrer: '',
   keywords: [],
   robots: {}
